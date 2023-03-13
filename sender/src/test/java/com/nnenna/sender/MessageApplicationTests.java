f16f85fcf066cfa3ed78c47e8b2b3d677702cf9c
Young Maryclaret Nnenna <claretyoung@gmail.com>
2023-03-13 16:46:44 +0000

    Containerise services

--- sender/src/test/java/com/nnenna/sender/MessageApplicationTests.java
+++ sender/src/test/java/com/nnenna/sender/MessageApplicationTests.java
@@ -1,13 +1,13 @@
-package com.nnenna.sender;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MessageApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.nnenna.sender;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MessageApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
